74396f99e975f8657f8ebb0d80ff0ebe1cde57ed
Benjamin Golder <benjamin.j.golder@gmail.com>
2013-01-12 17:35:03 -0500

    added javascript elements and templates

---
+++ lottery/templates/js/interviews_navigation.js
@@ -0,0 +1,23 @@
+
+
+function goToGrid(){
+}
+
+function goToMap(){
+}
+
+function goToNextInterview( thisInterview ){
+}
+
+function goToPrevInterview( thisInterview ){
+}
+
+function goToDetail( number ){
+}
+
+function goToSplit( number ){
+}
+
+function goToMapHighlight( number ){
+}
+
