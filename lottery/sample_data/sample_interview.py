51c473c46b4c7fd1cffc2e0ea0ce6ede0db20e5a
Benjamin Golder <benjamin.j.golder@gmail.com>
2013-01-18 15:33:21 -0500

    added sample interview

---
+++ lottery/sample_data/sample_interview.py
@@ -0,0 +1,16 @@
+questions = {
+"How many NY Lotto tickets do you sell each week?":["About 200.",
+"I sell less than most of the other bodegas because I'm in a residential area."],
+"What kinds of numbers do people pick?":["A lot of people have favorite numbers that they pick each time.",],
+"Do you play the lottery? Why or why not?":["Yeah, I get tickets every once in a while. It's fun.",
+    "I like to play with my daughter after she wins a basketball game. I let her scratch off the tickets. It's a celebratory ritual.", ],
+"Are there lottery regulars who buy tickets here?":["For sure. There's about a dozen people who come in all the time."],
+"How many tickets do people usually buy?":["Mostly people just buy either one ticket or a few. The regulars buy a lot of tickets."],
+"How much of the money (or what percentage of the money) that the store brings in for lottery tickets does the store get to keep?":[
+"Well most of it goes back to the lottery, but I get to keep about 10%." ],
+        }
+
+description = "Ray Hornsby, Lottery Retailer at Wyckoff Bodega"
+
+
+
