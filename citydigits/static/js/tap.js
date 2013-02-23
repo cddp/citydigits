2531e26e606ca89993c7c27fa25253356027dc82
Benjamin Golder <benjamin.j.golder@gmail.com>
2013-02-22 22:46:18 -0500

    tap tested on iPad and works!

--- citydigits/static/js/tap.js
+++ citydigits/static/js/tap.js
@@ -12,29 +12,20 @@ var Tap = (function(){
     tap.enableTap = function (jq, subselector) {
         // set the touchstart listener
         jq.on('touchstart mousedown', subselector, function (e) {
-            console.log('moused down');
             var cachedX = e.pageX;
             var cachedY = e.pageY;
-            console.log( 'x:', cachedX, 'y:', cachedY);
             tap.touching = true;
-            console.log('set touching to true');
             tap.lastTouchEvent = e;
             setTimeout(function(){
-                console.log('timeout done');
                 var currX = tap.lastTouchEvent.pageX;
                 var currY = tap.lastTouchEvent.pageY;
-                console.log('checking coords');
-                console.log( 'x:', currX, 'y:', currY);
                 var movedX = (Math.abs(cachedX - currX) > 1);
                 var movedY = (Math.abs(cachedY - currY) > 1);
-                console.log( 'moved? x:', movedX, 'y:', movedY);
                 var stillTouching = tap.touching;
-                console.log('still touching?:', stillTouching);
                 // if the touching has ended within 200 ms, 
                 // and is in the same place, fire the tap
                 if ( !stillTouching && !movedX && !movedY ) {
                     var target = $(e.currentTarget);
-                    console.log('triggering tap on:', target);
                     // fire the tap event
                     target.trigger('tap');
                 }
@@ -42,8 +33,6 @@ var Tap = (function(){
         });
         // set the touchend listener
         jq.on('touchend mouseup', subselector, function (e) {
-            console.log('mouseup');
-            console.log('set touching to false');
             tap.touching = false;
             tap.lastTouchEvent = e;
         });
